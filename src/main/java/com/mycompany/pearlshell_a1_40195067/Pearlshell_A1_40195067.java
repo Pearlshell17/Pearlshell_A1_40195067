a8663fd25d7638a70271ff2ee0a16cc42fe79484
40195067 <40195067@HY-R104-12.resource.uc>
2025-12-09 12:44:04 +0000

    setting up git repository

---
+++ src/main/java/com/mycompany/pearlshell_a1_40195067/Pearlshell_A1_40195067.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pearlshell_a1_40195067;
+
+/**
+ *
+ * @author 40195067
+ */
+public class Pearlshell_A1_40195067 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
